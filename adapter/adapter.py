52338d0f579b8ae9a25a8f14d2f18e216f4ca0cd
nick <nick.p.doyle@gmail.com>
2014-12-11 20:25:12 +1100

    comments and correct signature of abstract Adapter.encode()

--- adapter/adapter.py
+++ adapter/adapter.py
@@ -2,23 +2,23 @@
 class Adapter():
     @classmethod
     def decode(cls, datastring):
-        # string data -> return standard 'Message'
+        # Given string data from a device, decode -> return standard 'Message'
         raise NotImplementedError()
 
     @classmethod
-    def encode(cls, datastring):
-        # Message -> string data
-        # return standard 'Message'
+    def encode(cls, message):
+        # Given a Message, encode -> string data to send to device
         raise NotImplementedError()
 
     @classmethod
     def response_to(cls, datastring):
-        # Message -> string data
-        # return standard 'Message'
+        # For this adapter, get response (Message) to one datastring
         raise NotImplementedError()
 
     @classmethod
-    def detect(cls, data):
+    def detect(cls, datastring):
+        # Given a datastring, determine adapter type it's for
+        # and return this adapter's class
         from .tk102 import tk102
-        if tk102.decode(data):
+        if tk102.decode(datastring):
             return tk102
