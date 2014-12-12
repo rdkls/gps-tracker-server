1f22b613f0dc286491dfb91d1d3a203c0a78d3bb
nick <nick.p.doyle@gmail.com>
2014-12-12 13:48:50 +1100

    tests for multiple messages in one delimited datastring

--- tests.py
+++ tests.py
@@ -1,5 +1,10 @@
-
 from models import Message
+from models import GPSDevice
+
+tk102_heartbeat_string = '865328021048409;'
+tk102_location_low_string = 'imei:865328021048409,tracker,141210172556,0411959136,L,,,0BD4,,7A78,,,,,0,0,0.0%,,;'
+tk102_location_full_string = 'imei:865328021048409,tracker,141210110820,,F,030823.000,A,3745.9502,S,14458.2049,E,1.83,119.35,,0,0,0.0%,,;'
+tk102_init_string = '##,imei:865328021048409,A;'
 
 def test_message_fifo():
     imei = 'test1'
@@ -14,15 +19,12 @@ def test_message_fifo():
     Message(imei=imei, message_datastring='333').save()
 
     m = Message.dequeue_response(imei=imei)
-    print m
     assert('111' == m.message_datastring)
 
     m = Message.dequeue_response(imei=imei)
-    print m
     assert('222' == m.message_datastring)
 
     m = Message.dequeue_response(imei=imei)
-    print m
     assert('333' == m.message_datastring)
 
     m = Message.dequeue_response(imei=imei)
@@ -31,3 +33,34 @@ def test_message_fifo():
     assert(None == m)
     m = Message.dequeue_response(imei=imei)
     assert(None == m)
+
+
+def test_gpsdevice_sent():
+    d = GPSDevice()
+    d.sent(tk102_init_string)
+    d.sent(tk102_heartbeat_string)
+    d.sent(tk102_location_full_string)
+    assert(d.imei)
+
+def test_gpsdevice_tk102_response_init():
+    d = GPSDevice()
+    d.sent(tk102_init_string)
+    assert(d.imei)
+    r = d.pop_response()
+    assert('LOAD'==r)
+
+def test_gpsdevice_tk102_response_multi():
+    d = GPSDevice()
+    d.sent(tk102_init_string + tk102_heartbeat_string)
+    assert(d.imei)
+    r = d.pop_response()
+    assert('LOAD'==r)
+    r = d.pop_response()
+    assert('ON'==r)
+
+def test_gpsdevice_tk102_response_hb():
+    d = GPSDevice()
+    d.sent(tk102_heartbeat_string)
+    assert(d.imei)
+    r = d.pop_response()
+    assert('ON'==r)
