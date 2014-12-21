9f77fc8cfb55e323a95b8a7a9a87f5a0a809ee67
nick <nick.p.doyle@gmail.com>
2014-12-22 10:28:09 +1100

    tmux serve dev + gulp

--- serve_gps_dev.sh
+++ serve_gps_dev.sh
@@ -8,9 +8,14 @@ if [ $? != 0 ] ; then
     tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps/www' C-m
     tmux send-keys -t ${SESSION_NAME} 'python -m SimpleHTTPServer 9898' C-m
 
-    tmux split-window -h -t ${SESSION_NAME}
+    tmux split-window -t ${SESSION_NAME}
     tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps' C-m
     tmux send-keys -t ${SESSION_NAME} '. venv/gps/bin/activate' C-m
     tmux send-keys -t ${SESSION_NAME} './rest_api/rest_api.py' C-m
+
+    tmux split-window -t ${SESSION_NAME}
+    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps/www' C-m
+    tmux send-keys -t ${SESSION_NAME} 'gulp watch' C-m
+
 fi
 tmux attach -t ${SESSION_NAME}
