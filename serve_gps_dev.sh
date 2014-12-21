b201d212a1f1b4f47ed5a0eca6162e7e568638b9
nick <nick.p.doyle@gmail.com>
2014-12-22 10:25:31 +1100

    tmux script for dev serving

---
+++ serve_gps_dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SESSION_NAME=gps_serve
+
+tmux has-session -t ${SESSION_NAME} 2>/dev/null
+if [ $? != 0 ] ; then
+    tmux new-session -s ${SESSION_NAME} -d
+
+    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps/www' C-m
+    tmux send-keys -t ${SESSION_NAME} 'python -m SimpleHTTPServer 9898' C-m
+
+    tmux split-window -h -t ${SESSION_NAME}
+    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps' C-m
+    tmux send-keys -t ${SESSION_NAME} '. venv/gps/bin/activate' C-m
+    tmux send-keys -t ${SESSION_NAME} './rest_api/rest_api.py' C-m
+fi
+tmux attach -t ${SESSION_NAME}
