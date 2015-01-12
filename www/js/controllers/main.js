ebb629d4b4f24940c2871c689aefd2a5d1ba74bd
nick <nick.p.doyle@gmail.com>
2015-01-12 11:18:21 +1100

    login/register error condition handling

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -30,6 +30,10 @@ angular.module('app').controller('MainCtrl', function (
     }
 
     $scope.login = function() {
+        if(!$scope.login_user.email.match(/^[^@]+@\w+\.\w+$/)) {
+            alert("That's not a valid email");
+            return;
+        }
         // Api ref is passed in here to avoid circular dependencies between User-Api
         User.login(Api, $scope.login_user.email, $scope.login_user.password)
         .then(function(data) {
@@ -38,14 +42,23 @@ angular.module('app').controller('MainCtrl', function (
     }
 
     $scope.register = function() {
+        if(!$scope.register_user.email.match(/^[^@]+@\w+\.\w+$/)) {
+            alert("That's not a valid email");
+            return;
+        }
         if($scope.register_user.password != $scope.register_user.password_again) {
             alert('Passwords must match');
         }
         else {
             User.register(Api, $scope.register_user.email, $scope.register_user.password)
-            .then(function(data) {
-                $location.url('/dash');
-            });
+            .then(
+                function(data) {
+                    $location.url('/dash');
+                },
+                function(data) {
+                    alert("Sorry, there was a problem registering that email. Perhaps youre' already registered; try logging in.");
+                }
+            );
         }
     };
 
